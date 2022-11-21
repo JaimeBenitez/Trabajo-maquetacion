1d7d59957bce5560363bfbd6f6cf5266630b9d63
jaime <jbenaci736@g.educaaand.es>
2022-11-21 16:38:54 +0100

    --Añadido boton de cambio de modo

---
+++ dist/index.159a463a.js
@@ -0,0 +1,8 @@
+const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+function cambiaTema(ev) {
+    if (ev.target.checked) document.documentElement.setAttribute("tema", "light");
+    else document.documentElement.setAttribute("tema", "dark");
+}
+colorSwitch.addEventListener("change", cambiaTema);
+
+//# sourceMappingURL=index.159a463a.js.map
